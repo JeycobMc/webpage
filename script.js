3a29f5a907d4d086821da51bd59648e14486cc82
Jeycob <2423010896@untels.edu.pe>
2025-05-10 18:46:42 -0500

    last changes

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ const title = document.getElementById("title");
 function hello () {
     console.log("Hello");
     title.innerText = "jeycob haz estructuras";
-}
+}   
 btn.addEventListener("click", hello);
+const input = document.getElementById("input");
+input.addEventListener("input",(e)=>{
+    console.log(e.target.value);
+    title.innerText = e.target.value;
+});
+
+//---------------
+const prompt = document.getElementById("prompt");
+const generate = document.getElementById("generate");
+const image = document.getElementById("image");
+
+generate.addEventListener("click", () => { 
+    const imgSrc = "https://image.pollinations.ai/prompt/" + prompt.value;
+    image.src = imgSrc;
+});
+
+
